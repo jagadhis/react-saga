e1b15c6b06360ce97a4944866ff04981afb7ecd5
jagadhis <jwaran78@gmail.com>
2022-05-26 13:25:35 +0530

    initialized State

---
+++ src/counter.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+
+const counter = () => {
+  const [count, setcount] = useState(0);
+  const increment = () => {
+    setcount(count + 1);
+  };
+
+  const decrement = () => {
+    setcount(count - 1);
+  };
+
+  return;
+
+  <div></div>;
+};
