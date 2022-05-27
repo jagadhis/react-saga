7c43fb50a97250d2d6a690381f2fb155b3bf4286
jagadhis <jwaran78@gmail.com>
2022-05-27 16:32:40 +0530

    created New

--- saga/src/redux/store.js
+++
@@ -1,7 +0,0 @@
-import { createStore, compose } from "redux";
-
-const store = compose(window.devToolsExtension && window.devToolsExtension())(
-  createStore
-)(rootReducer);
-
-export default store;
