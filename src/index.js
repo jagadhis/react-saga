b2141b0dd4b5234bd7bfc1c5626a1e6af58137ea
jagadhis <jwaran78@gmail.com>
2022-05-27 11:51:17 +0530

    added saga folder again

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import store from "./sagamiddleware/configureStore";
 import { Provider } from "react-redux";
-import store from "./redux/configureStore";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
