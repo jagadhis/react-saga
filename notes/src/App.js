09b783309d159aa7a703f8f4d7d8f4a0bc0cdf0b
jagadhis <jwaran78@gmail.com>
2022-05-28 11:50:07 +0530

    created  a pp on reactV18

--- notes/src/App.js
+++ notes/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Notes from "./components/Notes";
+import "./style.css";
 
-function App() {
+export default function App() {
+  let [list, setList] = useState([]);
+  let [newNote, setNewNote] = useState("");
+
+  function addNote() {
+    let addedNote = list.concat(newNote);
+    setList(addedNote);
+    setNewNote("");
+  }
+
+  function handleChange(e) {
+    setNewNote(e.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>My Notes</h1>
+      <input type="text" value={newNote} onChange={handleChange} />
+
+      <button onClick={addNote}>Save</button>
+      <Notes data={list} />
     </div>
   );
 }
-
-export default App;
